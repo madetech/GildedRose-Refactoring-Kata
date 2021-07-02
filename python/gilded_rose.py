eb2d8cf704b452f1fa948151366bd81f070e6043
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2021-07-02 14:48:59 +0100

    condensed if not brie or concert tickets

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -16,6 +16,7 @@ class GildedRose(object):
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
                     item.quality = item.quality - 1
+
             # Item is Aged Brie or backstage passes
             if item.name == "Aged Brie" or item.name == "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality < 50:  # => 49
@@ -29,10 +30,9 @@ class GildedRose(object):
                                 item.quality = item.quality + 1
 
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            item.quality = item.quality - 1
+                if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+                    if item.quality > 0:
+                        item.quality = item.quality - 1
                 if item.name == "Backstage passes to a TAFKAL80ETC concert":
                     item.quality = 0
                 if item.name == "Aged Brie":
