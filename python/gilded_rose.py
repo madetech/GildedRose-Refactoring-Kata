0d064514dd49ae86f291e1e7413abc27ddfcd2f7
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2021-07-02 11:42:22 +0100

    Remove conditional for sulfuras for post sale by date quality reduction

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -28,8 +28,7 @@ class GildedRose(object):
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
                         if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
+                            item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
                 else:
