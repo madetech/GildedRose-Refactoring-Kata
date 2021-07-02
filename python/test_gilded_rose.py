9b05f61b5f5e6d1349a201f4afe93571ff0978a9
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2021-07-02 11:25:31 +0100

    Add approval tests for 2 and 16 days

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,16 +1,42 @@
 # -*- coding: utf-8 -*-
 import unittest
 
+from approvaltests.approvals import verify
 from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+    items = [
+        Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
+        Item(name="Aged Brie", sell_in=2, quality=0),
+        Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+        Item(name="Backstage passes to a TAFKAL80ETC concert",
+             sell_in=15, quality=20),
+        Item(name="Backstage passes to a TAFKAL80ETC concert",
+             sell_in=10, quality=49),
+        Item(name="Backstage passes to a TAFKAL80ETC concert",
+             sell_in=5, quality=49),
+        Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
+    ]
 
-        
+    def test_verify_update_quality(self):
+        GildedRose(self.items).update_quality()
+        verify(self.items)
+
+    def test_verify_update_quality_multiple_days(self):
+        days = 15
+        for day in range(days):
+            GildedRose(self.items).update_quality()
+        verify(self.items)
+
+        # def test_foo(self):
+        #     items = [Item("foo", 0, 0)]
+        #     gilded_rose = GildedRose(items)
+        #     gilded_rose.update_quality()
+        #     self.assertEquals("foo", items[0].name)
+
+
 if __name__ == '__main__':
     unittest.main()
