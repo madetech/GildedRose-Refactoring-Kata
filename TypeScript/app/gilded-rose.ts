be5efa8117eae9f37d7fc3e295a1ee28bb144310
MadeTech Dushan <dushan@madetech.com>
2021-07-02 14:19:26 +0100

    remove sulfuras if block 2

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -43,9 +43,7 @@ export class GildedRose {
                     }
                 }
             }
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
-            }
+            this.items[i].sellIn = this.items[i].sellIn - 1;
             if (this.items[i].sellIn < 0) {
                 if (this.items[i].name != 'Aged Brie') {
                     if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
