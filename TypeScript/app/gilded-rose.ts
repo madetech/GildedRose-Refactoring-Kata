cb7036835723fdc5f5dd9a49c240f80f53a12c20
MadeTech Dushan <dushan@madetech.com>
2021-07-02 15:44:18 +0100

    rename reduce quality method

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -25,7 +25,7 @@ export class GildedRose {
                 continue;
             }
             if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                this.reduceQuality(this.items[i]);
+                this.reduceQualityIfQualityAboveMinimum(this.items[i]);
             } else {
                 this.increaseQualityIfQualityUnderCap(this.items[i]);
 
@@ -43,7 +43,7 @@ export class GildedRose {
             if (this.items[i].sellIn < 0) {
                 if (this.items[i].name != 'Aged Brie') {
                     if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        this.reduceQuality(this.items[i]);
+                        this.reduceQualityIfQualityAboveMinimum(this.items[i]);
                     } else {
                         this.items[i].quality = 0;
                     }
@@ -56,7 +56,7 @@ export class GildedRose {
         return this.items;
     }
 
-    reduceQuality(item) {
+    reduceQualityIfQualityAboveMinimum(item) {
         if (item.quality > 0) {
             item.quality = item.quality -1;
         }
