9cce6b0fb02f43c25c8656263f015eafae1f5d68
MadeTech Dushan <dushan@madetech.com>
2021-07-02 15:37:52 +0100

    extract reduce quality method

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -25,9 +25,7 @@ export class GildedRose {
                 continue;
             }
             if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if (this.items[i].quality > 0) {
-                    this.items[i].quality = this.items[i].quality - 1
-                }
+                this.reduceQuality(this.items[i]);
             } else {
                 this.increaseQualityIfQualityUnderCap(this.items[i]);
 
@@ -45,9 +43,7 @@ export class GildedRose {
             if (this.items[i].sellIn < 0) {
                 if (this.items[i].name != 'Aged Brie') {
                     if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].quality > 0) {
-                            this.items[i].quality = this.items[i].quality - 1
-                        }
+                        this.reduceQuality(this.items[i]);
                     } else {
                         this.items[i].quality = 0;
                     }
@@ -60,6 +56,11 @@ export class GildedRose {
         return this.items;
     }
 
+    reduceQuality(item) {
+        if (item.quality > 0) {
+            item.quality = item.quality -1;
+        }
+    }
 
     increaseQualityIfQualityUnderCap(item) {
         if (item.quality < MAX_QUALITY) {
