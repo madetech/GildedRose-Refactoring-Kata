4a1a9163423784da9c67245b9f8d8d23aea709eb
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2021-07-02 11:41:23 +0100

    Remove conditional for sulfuras on item quality

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -12,8 +12,7 @@ class GildedRose(object):
                 continue
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                    item.quality = item.quality - 1
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
