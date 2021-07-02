83e5e0d95ea547ca0b2c3c5d8190bbc5499c83d4
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2021-07-02 14:45:44 +0100

    if concert tickets instead of else

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -33,8 +33,8 @@ class GildedRose(object):
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
                         if item.quality > 0:
                             item.quality = item.quality - 1
-                    else:
-                        item.quality = 0
+                if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    item.quality = 0
                 if item.name == "Aged Brie":
                     if item.quality < 50:
                         item.quality = item.quality + 1
