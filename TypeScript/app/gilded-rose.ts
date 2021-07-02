a862a52b76c4301dc1f8d585cb14f82794d443a0
MadeTech Dushan <dushan@madetech.com>
2021-07-02 14:36:39 +0100

    Implement qualityLessThanFifty() function

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -27,16 +27,16 @@ export class GildedRose {
                     this.items[i].quality = this.items[i].quality - 1
                 }
             } else {
-                if (this.items[i].quality < 50) {
+                if (this.qualityLessThanFifty(this.items[i])) {
                     this.items[i].quality = this.items[i].quality + 1
                     if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
                         if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
+                            if (this.qualityLessThanFifty(this.items[i])) {
                                 this.items[i].quality = this.items[i].quality + 1
                             }
                         }
                         if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
+                            if (this.qualityLessThanFifty(this.items[i])) {
                                 this.items[i].quality = this.items[i].quality + 1
                             }
                         }
