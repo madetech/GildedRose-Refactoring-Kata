bb95f1009f3309cee0979acc6a795e2627be0605
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2021-07-02 13:32:43 +0100

    Added clarity through spacing

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -10,6 +10,7 @@ class GildedRose(object):
 
             if item.name == "Sulfuras, Hand of Ragnaros":
                 continue
+
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
                     item.quality = item.quality - 1
@@ -23,7 +24,9 @@ class GildedRose(object):
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
+
             item.sell_in = item.sell_in - 1
+
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
